d4b12dc27788acc825810b5c6e15040dca3e6bc5
Luzardo99 <ruedaluzardo@gmail.com>
2021-04-25 16:31:28 -0500

    Update find.c

--- find.c
+++ find.c
@@ -39,9 +39,10 @@ void read_sub(char* sub_dir, char **argv)
 	char * temp_full_path=malloc(sizeof(char)*2000);
 	temp_full_path=strcpy(temp_full_path,sub_dir);
 	strcat(temp_full_path,temp_sub);
-    if (argv[3] == "name"){
+    if (argv[3] == "-n"){
           printf("sup bitch");
     }
+    printf("sup bitch");
     printf("%s\n", temp_full_path);
  	DIR * subsubdp=opendir(temp_full_path);
 	if(subsubdp!=NULL){
